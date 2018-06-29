7a683fe8ca857192e318f70066cdfd19de93f13b
Jarek <jjankowski1@gmail.com>
2018-06-29 18:05:45 +0200

    nexst step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
+
+        Person person1 = new Person();
+        person1.setFirstName("Jarek");
+        person1.setLastName("Browarek");
+        person1.setAge(18);
+        person1.setHeight(177);
+        person1.setHobby(new String[] {"Java", "Tennis", "Swimming"});
+
+        System.out.println(person1);
+
+        Gson gson = new Gson();
+        
 
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Person {
     String firstName;
     String lastName;
@@ -5,4 +7,54 @@ public class Person {
     int height;
     String[] hobby;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String[] getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String[] hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", hobby=" + Arrays.toString(hobby) +
+                '}';
+    }
 }
